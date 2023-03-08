cd1e109d7f782580aa1dfa1db48a593852988a7f
Martin Haynes <martin.haynes@focusrite.com>
2023-03-08 15:51:27 +0000

    Allow Visualizars to be chained

--- src/components/VisualizerWrapper/VisualizerWrapepr.tsx
+++ src/components/VisualizerWrapper/VisualizerWrapepr.tsx
@@ -1,17 +1,48 @@
-import {FC} from 'react';
+import {FC, useEffect, useState} from 'react';
 import {useAudioContext} from '../../contexts/audio-context';
 import {Visualizer} from '../Visualizer';
+export interface VisualIzerWrapperProps {
+  name: string;
+  inputNode: string;
+}
 
-export const VisualIzerWrapper: FC = () => {
+export const VisualIzerWrapper: FC<VisualIzerWrapperProps> = ({
+  name,
+  inputNode,
+}) => {
   const audioContextContext = useAudioContext();
+  const [audioContext, setAudioContext] = useState<AudioContext | undefined>(
+    undefined
+  );
+  const [audioInput, setAudioInput] = useState<AudioNode | undefined>(
+    undefined
+  );
 
-  const audioContext = audioContextContext?.audioContext;
-  const audioInput = audioContextContext?.getNode('input');
+  const [gain, setGain] = useState<GainNode | undefined>(undefined);
+
+  useEffect(() => {
+    if (audioContextContext?.audioContext) {
+      const audioContext = audioContextContext?.audioContext;
+
+      setAudioContext(audioContext);
+      setAudioInput(audioContextContext?.getNode(inputNode));
+
+      const gain = audioContext?.createGain();
+      gain?.gain.setValueAtTime(1, audioContext?.currentTime);
+      audioContextContext.addNode(name, gain);
+
+      setGain(gain);
+    }
+  }, [audioContextContext?.audioContext]);
 
   return (
     <>
-      {audioContext && audioInput && (
-        <Visualizer audioNodeIn={audioInput} audioContext={audioContext} />
+      {audioContext && audioInput && gain && (
+        <Visualizer
+          audioNodeIn={audioInput}
+          audioContext={audioContext}
+          audioNodeOut={gain}
+        />
       )}
     </>
   );
