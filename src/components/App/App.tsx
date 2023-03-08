2b92c5b1c78025c9acefd903e5f366f56f79a52a
Martin Haynes <martin.haynes@focusrite.com>
2023-03-08 16:39:55 +0000

    Revert "Allow Visualizars to be chained"
    This reverts commit cd1e109d7f782580aa1dfa1db48a593852988a7f.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,18 @@
-import {FC} from 'react';
-import styled from 'styled-components';
-import {AudioContextProvider} from '../../contexts/audio-context';
-import {InputSelector} from '../InputSelector';
-import {VisualIzerWrapper} from '../VisualizerWrapper';
+import { FC } from 'react'
+import styled from 'styled-components'
+import { AudioContextProvider } from '../../contexts/audio-context'
+import { InputSelector } from '../InputSelector'
+import { VisualIzerWrapper } from '../VisualizerWrapper'
 
 export const App: FC = () => {
-  return (
-    <AppWrapper>
-      <AudioContextProvider>
-        <InputSelector />
-        <VisualIzerWrapper name="scope" inputNode="input" />
-      </AudioContextProvider>
-    </AppWrapper>
-  );
-};
+  return <AppWrapper>
+    <AudioContextProvider>
+      <InputSelector />
+      <VisualIzerWrapper />
+    </AudioContextProvider>
+  </AppWrapper>
+}
 
-const AppWrapper = styled.div``;
+const AppWrapper = styled.div`
+  
+`
--- src/components/VisualizerWrapper/VisualizerWrapepr.tsx
+++ src/components/VisualizerWrapper/VisualizerWrapepr.tsx
@@ -1,48 +1,17 @@
-import {FC, useEffect, useState} from 'react';
+import {FC} from 'react';
 import {useAudioContext} from '../../contexts/audio-context';
 import {Visualizer} from '../Visualizer';
-export interface VisualIzerWrapperProps {
-  name: string;
-  inputNode: string;
-}
 
-export const VisualIzerWrapper: FC<VisualIzerWrapperProps> = ({
-  name,
-  inputNode,
-}) => {
+export const VisualIzerWrapper: FC = () => {
   const audioContextContext = useAudioContext();
-  const [audioContext, setAudioContext] = useState<AudioContext | undefined>(
-    undefined
-  );
-  const [audioInput, setAudioInput] = useState<AudioNode | undefined>(
-    undefined
-  );
 
-  const [gain, setGain] = useState<GainNode | undefined>(undefined);
-
-  useEffect(() => {
-    if (audioContextContext?.audioContext) {
-      const audioContext = audioContextContext?.audioContext;
-
-      setAudioContext(audioContext);
-      setAudioInput(audioContextContext?.getNode(inputNode));
-
-      const gain = audioContext?.createGain();
-      gain?.gain.setValueAtTime(1, audioContext?.currentTime);
-      audioContextContext.addNode(name, gain);
-
-      setGain(gain);
-    }
-  }, [audioContextContext?.audioContext]);
+  const audioContext = audioContextContext?.audioContext;
+  const audioInput = audioContextContext?.getNode('input');
 
   return (
     <>
-      {audioContext && audioInput && gain && (
-        <Visualizer
-          audioNodeIn={audioInput}
-          audioContext={audioContext}
-          audioNodeOut={gain}
-        />
+      {audioContext && audioInput && (
+        <Visualizer audioNodeIn={audioInput} audioContext={audioContext} />
       )}
     </>
   );
