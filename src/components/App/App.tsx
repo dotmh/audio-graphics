07401337ccbe8284c145df750ca4ff035f7f976b
Martin Haynes <martin.haynes@focusrite.com>
2023-03-08 09:47:12 +0000

    Tidy App

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import styled from 'styled-components'
+
+export const App: FC = () => {
+  return <AppWrapper>Audio Spectrum Analyser</AppWrapper>
+}
+
+const AppWrapper = styled.div`
+  
+`
